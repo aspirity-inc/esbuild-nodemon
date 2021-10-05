d305eac077258c25294cd73270c6fc9ad77d439b
Denis Zavershinskiy <dz@aspirity.com>
2021-10-06 01:07:51 +0700

    fix proper exit by ctrl+c

--- bin/esbuild-nodemon.js
+++ bin/esbuild-nodemon.js
@@ -6,43 +6,28 @@ const { rmdirSync } = require("../lib/rm");
 const { startBuild } = require("../lib/esbuild");
 const { startNodemon } = require("../lib/nodemon");
 
-async function run() {
-  const outFilename = "output.js";
-  const buildDir = resolvePath(getRootBuildDirectory(), `_${getPathDate()}`);
-  const outputFilePath = resolvePath(buildDir, outFilename);
-  console.log("[esbuild-nodemon]", { pid: process.pid });
+const outFilename = "output.js";
+const buildDir = resolvePath(getRootBuildDirectory(), `_${getPathDate()}`);
+const outputFilePath = resolvePath(buildDir, outFilename);
 
+function shutdown(signal) {
+  console.log("[esbuild-nodemon]", "Graceful shutdown", { signal });
+  const relPath = relative(process.cwd(), buildDir);
+  console.log("[esbuild-nodemon]", "Removing build directory", {
+    path: relPath,
+  });
+  rmdirSync(buildDir, { recursive: true });
+}
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+async function run() {
+  console.log("[esbuild-nodemon]", { pid: process.pid });
   /** @type { nodemon.Settings } */
   const nodemonOptions = nodemonParse(process.argv);
   await startBuild(nodemonOptions.script, outputFilePath);
-  const mon = startNodemon(nodemonOptions, outputFilePath);
-
-  function removeBuildDir() {
-    const relPath = relative(process.cwd(), buildDir);
-    console.log("[esbuild-nodemon]", "Removing build directory", {
-      path: relPath,
-    });
-    rmdirSync(buildDir, { recursive: true });
-  }
-
-  let shutdown = function (signal) {
-    console.log("[esbuild-nodemon]", "Graceful shutdown", { signal });
-
-    mon.emit("quit", 0);
-    mon.once("exit", () => {
-      removeBuildDir();
-      process.exit();
-    });
-
-    shutdown = () => {
-      removeBuildDir();
-      console.log("[esbuild-nodemon]", "Force shutdown");
-      process.exit();
-    };
-  };
-
-  process.on("SIGINT", shutdown);
-  process.on("SIGTERM", shutdown);
+  startNodemon(nodemonOptions, outputFilePath);
 }
 
 run();
